b9917c39008ce6ea09cffa96f1ffa963abf84b19
rajithst <rajiththennakoonshady@gmail.com>
2021-06-18 15:27:52 +0900

    quick-sort implementation

---
+++ sorting/quick_sort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+List = [3, 4, 2, 6, 5, 7, 1, 9]
+print(quick_sort(List))
